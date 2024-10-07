4282bff4a242cd4d2c37e67e09a763ef164175ec
bagus.candrabudi <bagus.candrabudi@gmail.com>
2024-10-07 17:39:06 +0700

    update route sidenav

--- resources/views/layouts/componentns/sidenav.blade.php
+++ resources/views/layouts/componentns/sidenav.blade.php
@@ -12,7 +12,7 @@
         </li>
         <li class="side-nav__divider my-6"></li>
         <li>
-            <a href="enigma-side-menu-inbox-page.html" class="side-menu">
+            <a href="{{ route('member.list') }}" class="side-menu">
                 <div class="side-menu__icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="users" class="lucide lucide-users stroke-1.5 w-5 h-5 mx-auto block"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                 </div>
@@ -118,7 +118,7 @@
             </a>
             <ul class="">
                 <li>
-                    <a href="enigma-side-menu-crud-data-list-page.html" class="side-menu">
+                    <a href="{{ route('paymentaccount.list') }}" class="side-menu">
                         <div class="side-menu__icon">
                             <i data-tw-merge="" data-lucide="activity" class="stroke-1.5 w-5 h-5"></i>
                         </div>
@@ -128,7 +128,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="enigma-side-menu-crud-form-page.html" class="side-menu">
+                    <a href="{{ route('payment.list') }}" class="side-menu">
                         <div class="side-menu__icon">
                             <i data-tw-merge="" data-lucide="activity" class="stroke-1.5 w-5 h-5"></i>
                         </div>
